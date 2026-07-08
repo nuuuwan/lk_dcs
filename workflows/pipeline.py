0b7747f4ece288efb50c0898b4dcf019fb1e02e7
nuuuwan <nuuuwan@gmail.com>
2026-07-08 10:06:04 +0530

    Refactor pipeline script to include logging and improve build process visibility

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,6 +1,11 @@
 from dcs import DocFactory, GlobalReadMe
+from utils_future import Log
 
-if __name__ == '__main__':
-    for cls in DocFactory.list():
+log = Log("pipeline")
+if __name__ == "__main__":
+    for i_cls, cls in enumerate(DocFactory.list(), start=1):
+        log.info(
+            f"[{i_cls}/{len(DocFactory.list())}] Building {cls.get_name()}"
+        )
         cls.build_latest()
     GlobalReadMe.build()
