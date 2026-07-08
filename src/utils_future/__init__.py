e259707b3b368eaa8f1714a812f6b0d9770fa662
nuuuwan <nuuuwan@gmail.com>
2026-07-08 09:35:18 +0530

    Add TimeFormat import to utils_future module

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -3,4 +3,5 @@ from utils_future.console.Log import Log
 from utils_future.ds.Parse import Parse
 from utils_future.file.JSONFile import JSONFile
 from utils_future.file.PDFFile import PDFFile
+from utils_future.time.TimeFormat import TimeFormat
 from utils_future.WWW import WWW
