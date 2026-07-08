86bddeb8af516ab34531cc770766fe5fe1828180
nuuuwan <nuuuwan@gmail.com>
2026-07-08 12:14:52 +0530

    Update README files with latest checked timestamps and enhance validation logic in CCPISubgroupsValidateMixin

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -4,8 +4,6 @@ from utils_future import Log
 log = Log("pipeline")
 if __name__ == "__main__":
     for i_cls, cls in enumerate(DocFactory.list(), start=1):
-        if i_cls != 3:
-            continue
         print("-" * 32)
         log.info(
             f"[{i_cls}/{len(DocFactory.list())}] Building {cls.get_name()}"
