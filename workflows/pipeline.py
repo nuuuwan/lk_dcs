711a2223d8fc08712b5a28a895bd47336621a89e
nuuuwan <nuuuwan@gmail.com>
2026-07-08 12:42:22 +0530

    Update last checked timestamps in README files and fix typo in pipeline script

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -4,10 +4,11 @@ from utils_future import Log
 log = Log("pipeline")
 if __name__ == "__main__":
     for i_cls, cls in enumerate(DocFactory.list(), start=1):
-        print("-" * 32)
+        log.debug("-" * 60)
         log.info(
             f"[{i_cls}/{len(DocFactory.list())}] Building {cls.get_name()}"
         )
-        print("-" * 32)
+        log.debug("-" * 60)
         cls.build_latest()
     GlobalReadMe.build()
+S
